993b63174b4ee38508d870c3b9cbe68f92758608
MORGUNOV <academic08rostov@mail.ru>
2021-04-04 21:14:13 +0300

    использовал классы

---
+++ 034/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _034
+{
+    class Point
+    {
+       public int x,y;
+       public char c;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
--- 034/Program.cs
+++ 034/Program.cs
@@ -8,15 +8,25 @@ namespace _034
         {
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
-            Draw(2, 3, '*');
+            Point point = new Point();
+            point.x = 2;
+            point.y = 3;
+            point.c = '*';
+            point.Draw();
+
+            Point point1 = new Point();
+            point1.x = 4;
+            point1.y = 5;
+            point1.c = '*';
+            point1.Draw();
 
             Console.ReadKey();
         }
 
-        static void Draw(int x,int y,char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-        }
+        //static void Draw(int x,int y,char c)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.Write(c);
+        //}
     }
 }
